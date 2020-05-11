b1f47d23b9c93fb042eb43d4f0ccc0d5911e6e97
ashutosh1ibm <ashutoshk5029@gmail.com>
2020-05-11 15:58:36 +0530

    ProductDTo changes

--- src/main/java/com/example/devopscasestudy/api/ProductAPI.java
+++ src/main/java/com/example/devopscasestudy/api/ProductAPI.java
@@ -1,8 +1,13 @@
 package com.example.devopscasestudy.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,52 +23,92 @@ import com.example.devopscasestudy.repo.ProductRepository;
 
 @RestController
 public class ProductAPI {
-	
-	@Autowired
-	private ProductRepository productRepository;
-	
-	//node express js
-	//app.get('/products',(req,res=>))
-	
-	@GetMapping("/products")
-	public ResponseEntity<List<Product>> findAll(){
-		List<Product> products=productRepository.findAll();
-		return new ResponseEntity<>(products, HttpStatus.OK);
-	}
-	
-	//https://localhost:8081/products/5674
-	@GetMapping("/products/{price}")
-	public ResponseEntity<List<Product>> findByPrice(@PathVariable("price")double price){
-		List<Product> products=productRepository.findByPriceGreaterThan(price);
-		return new ResponseEntity<>(products, HttpStatus.OK);
-	}
-	
-	@GetMapping("/products/find/{name}")
-	public ResponseEntity<List<Product>> findByName(@PathVariable("name")String name){
-		List<Product> products=productRepository.findByProductNameIgnoreCase(name);
-		return new ResponseEntity<>(products, HttpStatus.OK);
-	}
-	
-	//http://localhost:8081/products/5674
-    @DeleteMapping("/products/{productId}")
-    
-    public @ResponseBody ResponseEntity<StringResponse> delete(@PathVariable("productId")int productId){
-        productRepository.deleteById(productId);
-        
- 
-       
-    
-        return new ResponseEntity<>(new StringResponse("Deleted Order "+productId), HttpStatus.OK);
-   
-    }
-	
-	// app.get('/products',(req,res))
-	
-	
-	@PostMapping("/products")
-	public ResponseEntity<Product> save(@RequestBody Product product){
-		productRepository.save(product);
-		return new ResponseEntity<Product>(product,HttpStatus.CREATED);
-	}
-	
+
+private final Logger logger = LoggerFactory.getLogger(getClass());
+
+@Autowired
+private ProductRepository productRepository;
+
+@Autowired
+private ModelMapper modelMapper;
+
+
+// node express js
+// app.get('/products',(req,res)=>)
+
+@GetMapping("/products")
+public ResponseEntity<List<ProductDTO>> findAll(){
+logger.info("Processing findAll request");
+List<Product> products=productRepository.findAll();
+
+
+return new ResponseEntity<>(products.stream()
+.map(this::convertToDto)
+.collect(Collectors.toList()), HttpStatus.OK);
+}
+
+//http://localhost:8081/products/5674
+@GetMapping("/products/{price}")
+public ResponseEntity<List<ProductDTO>> findByPrice(@PathVariable("price")double price){
+List<Product> products=productRepository.findByPriceGreaterThan(price);
+return new ResponseEntity<>(products.stream()
+.map(this::convertToDto)
+.collect(Collectors.toList()), HttpStatus.OK);
+}
+
+@GetMapping("/products/find/{name}")
+public ResponseEntity<List<ProductDTO>> findByName(@PathVariable("name")String name){
+
+logger.info("Processing findByName request");
+List<Product> products=productRepository.findByProductNameIgnoreCase(name);
+return new ResponseEntity<>(products.stream()
+.map(this::convertToDto)
+.collect(Collectors.toList()), HttpStatus.OK);
+}
+
+// "\"This is a String\""
+
+//http://localhost:8081/products/5674
+@DeleteMapping("/products/{productId}")
+
+public @ResponseBody ResponseEntity<StringResponse> delete(@PathVariable("productId")int productId){
+productRepository.deleteById(productId);
+
+
+
+
+
+return new ResponseEntity<>(new StringResponse("Deleted Order "+productId), HttpStatus.OK);
+
+}
+
+
+// app.post('/products',(req,res)=>)
+@PostMapping("/products")
+public ResponseEntity<ProductDTO> save(@RequestBody ProductDTO productDTO){
+Product product=this.convertToEntity(productDTO);
+
+productRepository.save(product);
+return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+}
+
+
+private ProductDTO convertToDto(Product product) {
+ProductDTO productDTO = modelMapper.map(product, ProductDTO.class);
+
+return productDTO;
+}
+
+private Product convertToEntity(ProductDTO productDTO) throws ParseException {
+Product product = modelMapper.map(productDTO, Product.class);
+
+if (productDTO.getProductId() != null) {
+Product oldProduct = productRepository.getOne(productDTO.getProductId());
+
+}
+return product;
+}
+
+
+
 }
